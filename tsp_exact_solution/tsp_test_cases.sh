ef42c349a5c87f71435f1f15fced6718c687cdaa
Alberto <jimeneeaxjw@dukes.jmu.edu>
2023-12-07 20:26:09 -0500

    test cases

--- tsp_exact_solution/tsp_test_cases.sh
+++ tsp_exact_solution/tsp_test_cases.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+# Function to extract wall clock time from time command output
+get_wall_clock_time() {
+  awk '/real/ {print $2}'
+}
+
 # Run the approximation solution on different test cases
 
 # Test Case 1: Small input
 echo "Running Test Case 1: Small input"
-python cs412_tsp_exact.py < test1/test1Input.txt > test1/test1Output.txt
+{ time python cs412_tsp_exact.py < test1/test1Input.txt; } 2>&1 | get_wall_clock_time | tee -a test1/test1Output.txt
 
 # Test Case 2: Medium input
 echo "Running Test Case 2: Medium input"
-python cs412_tsp_exact.py < test2/test2Input.txt > test2/test2Output.txt
+{ time python cs412_tsp_exact.py < test2/test2Input.txt; } 2>&1 | get_wall_clock_time | tee -a test2/test2Output.txt
 
 # Test Case 3: Large input
 echo "Running Test Case 3: Large input"
-python cs412_tsp_exact.py < test3/test3Input.txt > test3/test3Output.txt
+{ time python cs412_tsp_exact.py < test3/test3Input.txt; } 2>&1 | get_wall_clock_time | tee -a test3/test3Output.txt
 
 # Test Case 4: High complexity input
 echo "Running Test Case 4: High complexity input"
-python cs412_tsp_exact.py < test4/test4Input.txt > test4/test4Output.txt
+{ time python cs412_tsp_exact.py < test4/test4Input.txt; } 2>&1 | get_wall_clock_time | tee -a test4/test4Output.txt
 
 # Test Case 5: Worst case for approximation (does not achieve optimal answer)
 echo "Running Test Case 5: Worst case for approximation"
-python cs412_tsp_exact.py < test5/test5Input.txt > test5/test5Output.txt
+{ time python cs412_tsp_exact.py < test5/test5Input.txt; } 2>&1 | get_wall_clock_time | tee -a test5/test5Output.txt
 
 # Add more test cases as needed...
 
